8f7ea6e273fe31f7f6afc5036a167ba50938fd3e
Lesseguince Laurent <m.laurent974@hotmail.com>
2023-11-04 01:52:54 +0400

    extraire les noms des categories

--- src/scrapper.py
+++ src/scrapper.py
@@ -4,10 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 
 from src.consts import BASE_URL
-from src.file_manager import validate_data_dir  
+from src.consts import CATEGORY_URL
 from src.extractor import extract_categories
 
-def scrap_all_categorie(base_url=BASE_URL):
+def scrap_all_categories(base_url: str = BASE_URL) -> list:
     """Scarp all categories frome tje website"""
 
     #request la page
@@ -19,4 +19,33 @@ def scrap_all_categorie(base_url=BASE_URL):
     # extract categories form main page
     categ_list = extract_categories(soup)
 
+    # Extraire les noms des catégories à partir des URL
+    category_names = [CATEGORY_URL.split("/")[-2].split("_")[0] for url in categ_list]
+    print("Scraping all categories is done. Category names:", category_names)
+
     return categ_list
+
+
+
+
+
+# def scrap_all_urls(base_url):
+    #"""Scrap all URLs from the website and print them."""
+    # Envoyer une requête HTTP pour obtenir le contenu de la page principale
+    #response = requests.get(base_url)
+
+    # Analyser le contenu HTML de la page avec BeautifulSoup
+    #soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extraire tous les liens de catégorie
+    #links = soup.find_all('h3')
+
+    # Parcourir les liens et imprimer les URLs
+    #for link in links:
+            #href = link.a['href']
+            #print(href)
+
+    #else:
+        #print("La requête a échoué avec le code de statut", response.status_code)
+
+
