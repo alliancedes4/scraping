0816f0c402aa01bda000ac67ae528b97e891d896
Lesseguince Laurent <m.laurent974@hotmail.com>
2023-11-04 00:04:03 +0400

    correction erreur scrap_all-categories

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from src.scrapper import manage_all_categories 
-from src.scrapper import scrap__all_categories
+
+from src.scrapper import scrap_all_categories
+from src.consts import BASE_URL
+#from src.scrapper import scrap_all_urls
+
+
 def main():
     """main function of the program"""
 
     # 1 scrap toute les catégorie 
-    manage_all_categories()
-    # 2 pour chaque catégorie 
-    scrap__all_categories()
-    # je scrap les urls et je save
+    scrap_all_categories()
 
+    # 2 pour chaque catégorie 
+    
+    # je scrap les urls et je save
+    #scrap_all_urls()
     # 3 pour chaque url je scrap les infos et je save dans un csv
 
 if __name__ ==  "__main__":
